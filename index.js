8e3f2d5fdb635682b9961452d9529f72359cf621
jorge <galarza.jorgemiguel@iesconcepcion.edu.ar>
2023-12-14 18:48:25 -0300

    new project

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
-var numeros = [];
+const numeros = [];
 
-function validateInput() {
-  var numInput = document.getElementById("numInput").value;
-  var regex = /^[0-9]$/;
-
-  if (!regex.test(numInput)) {
-    alert("Por favor, ingresa solo un número.");
-    document.getElementById("numInput").value = "";
-  }
-}
-
-function agregarNumero() {
-  var numInput = document.getElementById("numInput").value;
+function agregarNumero() { // compureba si el valor esta vacio
+  const numInput = document.getElementById("numInput").value;
 
   if (numInput !== "") {
     numeros.push(parseInt(numInput));
-    document.getElementById("numerosIngresados").innerHTML = "Números ingresados:<br>" + numeros.join("<br>");
+    document.getElementById("numerosIngresados").innerHTML += "<li>" + numInput+ "</li>" ;
     document.getElementById("numInput").value = "";
     
-    if (numeros.length === 4) {
-      document.getElementById("numInput").disabled = true;
-    }
   }
 }
 
 function sumar() {
-  if (numeros.length === 4) {
-    var suma = numeros.reduce(function(acc, num) {
-      return acc + num;
-    }, 0);
+  let suma = 0;
+for (let index = 0; index < numeros.length; index++) {
+  suma += numeros[index];
+}
+  let maximo = Math.max.apply(null, numeros);
+  let minimo = Math.min.apply(null, numeros);
+let promedio = suma / numeros.length
 
-    var maximo = Math.max.apply(null, numeros);
-    var minimo = Math.min.apply(null, numeros);
-
-    document.getElementById("resultado").innerText = "La suma de los números es: " + suma;
-    document.getElementById("maximo").innerText = "El valor máximo es: " + maximo;
-    document.getElementById("minimo").innerText = "El valor mínimo es: " + minimo;
-  } else {
-    alert("Por favor, ingresa 4 números antes de calcular la suma.");
-  }
+  document.getElementById("resultado").innerText = "La suma de los números es: " + suma;
+  document.getElementById("promedio").innerText = "el promedio de los numeros es: " + promedio;
+  document.getElementById("maximo").innerText = "El valor máximo es: " + maximo;
+  document.getElementById("minimo").innerText = "El valor mínimo es: " + minimo;
 }
